325a6b7db68f25469e59ba1f81a5de4f349c7dc1
TranshumanSoft <53468949+TranshumanSoft@users.noreply.github.com>
2019-09-23 17:29:38 +0200

    Add files via upload

---
+++ modulexercises.py
@@ -0,0 +1,5 @@
+fstnumber = float(input("Introduce a number:"))
+scndnumber = float(input("Introduce another number:"))
+quotient = fstnumber//scndnumber
+rest = fstnumber%scndnumber
+print(f"Between {fstnumber} and {scndnumber} there's a quotient of {quotient} and a rest of {rest}")
